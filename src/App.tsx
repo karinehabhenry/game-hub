935dd077a6505d4c1083777ea43bbb51a10d4724
karinehabhenry <161515319+karinehabhenry@users.noreply.github.com>
2025-03-05 19:42:00 +0200

    Fetch Genres

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,28 @@ import { Button, Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import NavBar from "./component/NavBar";
 import { useColorMode } from "./components/ui/color-mode";
 import GameGrid from "./component/GameGrid";
+import GenreList from "./component/GenreList";
 
 const App = () => {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`,
-
-    }}>
-      <GridItem area='nav'>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
-      {useBreakpointValue({ base: null, lg: <GridItem area='aside'>Aside</GridItem> })}
-      <GridItem area='main'>
+      {useBreakpointValue({
+        base: null,
+        lg: <GridItem area="aside"><GenreList/></GridItem>,
+      })}
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
-  )
-}
+  );
+};
 
 export default App;
