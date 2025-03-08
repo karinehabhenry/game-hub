3032d41ddf2c6dbe4448c65882c7cc4dc513e0dc
karinehabhenry <161515319+karinehabhenry@users.noreply.github.com>
2025-03-08 16:24:29 +0200

    Cleanup the grenres

--- src/component/GenreList.tsx
+++ src/component/GenreList.tsx
@@ -1,30 +1,58 @@
 import useData from "@/hooks/useData";
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImgUrl from "@/services/image-url";
-import { Button, HStack, Image, List, ListIndicator, ListItem, ListRoot, Spinner, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListIndicator,
+  ListItem,
+  ListRoot,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 interface Props {
-    onSelectGenre: (genre: Genre) => void
-    selectedGenre: Genre | null;
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre, selectedGenre}:Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, loading, error } = useGenres();
 
-    if (error) return null;
+  if (error) return null;
   if (loading) return <Spinner />;
   return (
+    <>
+    <Heading fontSize={"2xl"} marginBottom={2}>
+      Genres
+    </Heading>
     <ListRoot>
       {data.map((genre) => (
-        <ListIndicator key={genre.id} paddingY='5px'>
+        <ListIndicator key={genre.id} paddingY="5px">
           <HStack>
-            <Image boxSize={'32px'} borderRadius={8} src={getCroppedImgUrl(genre.image_background)} />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' :'normal' } onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='ghost'>{genre.name}</Button>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              objectFit={"cover"}
+              src={getCroppedImgUrl(genre.image_background)}
+            />
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="ghost"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListIndicator>
       ))}
     </ListRoot>
+    </>
   );
 };
 
